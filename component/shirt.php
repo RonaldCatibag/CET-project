299137cd10e29ddfad70e214b5647832b1f53520
Ronald Catibag <catibagronald@gmail.com>
2021-02-04 00:02:55 +0800

    first-commit

---
+++ component/shirt.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/design.css?v=<?php echo time(); ?>">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="items">
+
+        <div class="item1">
+            <img src="../assets/items/1 (1).png" alt="item2" width="100%">
+            <div class="shop_now">
+                <h4>Buy now!</h4>
+            </div>
+        </div>
+
+        <div class="item1">
+            <img src="../assets/items/1 (2).png" alt="item2" width="100%">
+            <div class="shop_now">
+                <h4>Buy now!</h4>
+            </div>
+        </div>
+
+        <div class="item1">
+            <img src="../assets/items/1 (3).png" alt="item2" width="100%">
+            <div class="shop_now">
+                <h4>Buy now!</h4>
+            </div>
+        </div>
+
+
+        <div class="item1">
+            <img src="../assets/items/1 (5).png" alt="item2" width="100%">
+            <div class="shop_now">
+                <h4>Buy now!</h4>
+            </div>
+        </div>
+        <div class="item1">
+            <img src="../assets/items/1 (6).png" alt="item2" width="100%">
+            <div class="shop_now">
+                <h4>Buy now!</h4>
+            </div>
+        </div>
+        <div class="item1">
+            <img src="../assets/items/1 (7).png" alt="item2" width="100%">
+            <div class="shop_now">
+                <h4>Buy now!</h4>
+            </div>
+        </div>
+        <div class="item1">
+            <img src="../assets/items/1 (8).png" alt="item2" width="100%">
+            <div class="shop_now">
+                <h4>Buy now!</h4>
+            </div>
+        </div>
+
+
+</body>
+
+</html>
---
+++ main.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style/design.css?v=<?php echo time(); ?>">
+    <script src="https://kit.fontawesome.com/d0870bddbc.js" crossorigin="anonymous"></script>
+    <title>SHOP</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="main_content">
+            <div class="header">
+
+                <div class="logo"><img src="logo.svg" width="60" height="60" alt="" srcset=""> </div>
+                <div class="number_hours"> <button id="number"><a href="test">+63 961-202-8946 </button></a> <button id="number"><a href="test">CS Open 9:00 AM TO 6:00 PM </button> </a></div>
+                <div class="cal_button" id="test"> <button id="req-call"><a href="test">Request a Call </button></a></div>
+            </div>
+            <div class="tabs">
+                <ul>
+                    <li><i class="fas fa-tshirt"></i><a href="./component/shirt.php">SHIRTS</a></li>
+                    <li><a href="./component/shorts.php">SHORTS</a></li>
+                    <li><a href="./component/jeans.php">JEANS</a></li>
+                    <li><a href="./component/mugs.php">MUGS</a></li>
+                </ul>
+            </div>
+            <div class="main_info">
+                <img src="./assets/img/background.svg" alt="" srcset="">
+                <div class="about_product">
+
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+
+
+    </div>
+
+
+</body>
+
+</html>
