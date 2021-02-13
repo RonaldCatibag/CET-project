19d3085e980e420e013e3b476ca68fd95b2719a2
Mark Ashley Sombillo <markashley423@gmail.com>
2021-02-13 17:04:38 +0800

    Added popup

--- component/shirt.php
+++ component/shirt.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/design.css?v=<?php echo time(); ?>">
     <title>Document</title>
@@ -10,53 +11,83 @@
 
 <body>
     <div class="items">
+        <a href="#" id="button" class="button">
+            <div class="item1">
+                <img src="../assets/items/1 (1).png" alt="item2" width="100%">
+                <div class="shop_now">
+                    <h4>Buy now!</h4>
+                </div>
+            </div>
+        </a>
+        
+        <a href="#" id="button" class="button">
+            <div class="item1">
+                <img src="../assets/items/1 (2).png" alt="item2" width="100%">
+                <div class="shop_now">
+                    <h4>Buy now!</h4>
+                </div>
+            </div>
+        </a>
 
-        <div class="item1">
-            <img src="../assets/items/1 (1).png" alt="item2" width="100%">
-            <div class="shop_now">
-                <h4>Buy now!</h4>
+        <a href="#" id="button" class="button">
+            <div class="item1">
+                <img src="../assets/items/1 (3).png" alt="item2" width="100%">
+                <div class="shop_now">
+                    <h4>Buy now!</h4>
+                </div>
             </div>
-        </div>
+        </a>    
 
-        <div class="item1">
-            <img src="../assets/items/1 (2).png" alt="item2" width="100%">
-            <div class="shop_now">
-                <h4>Buy now!</h4>
+        <a href="#" id="button" class="button">
+            <div class="item1">
+                <img src="../assets/items/1 (5).png" alt="item2" width="100%">
+                <div class="shop_now">
+                    <h4>Buy now!</h4>
+                </div>
             </div>
-        </div>
+        </a>
 
-        <div class="item1">
-            <img src="../assets/items/1 (3).png" alt="item2" width="100%">
-            <div class="shop_now">
-                <h4>Buy now!</h4>
+        <a href="#" id="button" class="button">
+            <div class="item1">
+                <img src="../assets/items/1 (6).png" alt="item2" width="100%">
+                <div class="shop_now">
+                    <h4>Buy now!</h4>
+                </div>
             </div>
-        </div>
+        </a>
 
+        <a href="#" id="button" class="button">
+            <div class="item1">
+                <img src="../assets/items/1 (7).png" alt="item2" width="100%">
+                <div class="shop_now">
+                    <h4>Buy now!</h4>
+                </div>
+            </div>
+        </a>
 
-        <div class="item1">
-            <img src="../assets/items/1 (5).png" alt="item2" width="100%">
-            <div class="shop_now">
-                <h4>Buy now!</h4>
+        <a href="#" id="button" class="button">
+            <div class="item1">
+                <img src="../assets/items/1 (8).png" alt="item2" width="100%">
+                <div class="shop_now">
+                    <h4>Buy now!</h4>
+                </div>
             </div>
+        </a>
+
+            <!-- MODALS -->
+    
+
+    <div class="bg-modal">
+        <div class="modal-content">
+            <form action="">
+                <div class="close">+</div>
+
+                <a href="#" id="button" class="button">Buy Now!</a>
+                </form>
         </div>
-        <div class="item1">
-            <img src="../assets/items/1 (6).png" alt="item2" width="100%">
-            <div class="shop_now">
-                <h4>Buy now!</h4>
-            </div>
-        </div>
-        <div class="item1">
-            <img src="../assets/items/1 (7).png" alt="item2" width="100%">
-            <div class="shop_now">
-                <h4>Buy now!</h4>
-            </div>
-        </div>
-        <div class="item1">
-            <img src="../assets/items/1 (8).png" alt="item2" width="100%">
-            <div class="shop_now">
-                <h4>Buy now!</h4>
-            </div>
-        </div>
+    </div>
+
+    <script src="../resources/script.js"></script>
 
 
 </body>
