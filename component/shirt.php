8b7efd8a5c5559205be1e509056126b3fc0e3a8e
Mark Ashley Sombillo <markashley423@gmail.com>
2021-02-15 14:43:51 +0800

    added popup

--- component/shirt.php
+++ component/shirt.php
@@ -11,79 +11,106 @@
 
 <body>
     <div class="items">
-        <a href="#" id="button" class="button">
+        
             <div class="item1">
+            <a href="#" id="button" class="button">
                 <img src="../assets/items/1 (1).png" alt="item2" width="100%">
                 <div class="shop_now">
                     <h4>Buy now!</h4>
                 </div>
+            </a>
             </div>
-        </a>
-        
-        <a href="#" id="button" class="button">
+
             <div class="item1">
+            <a href="#" id="button1" class="button">
                 <img src="../assets/items/1 (2).png" alt="item2" width="100%">
                 <div class="shop_now">
                     <h4>Buy now!</h4>
                 </div>
+            </a>
             </div>
-        </a>
 
-        <a href="#" id="button" class="button">
             <div class="item1">
-                <img src="../assets/items/1 (3).png" alt="item2" width="100%">
-                <div class="shop_now">
-                    <h4>Buy now!</h4>
-                </div>
-            </div>
-        </a>    
-
-        <a href="#" id="button" class="button">
-            <div class="item1">
+            <a href="#" id="button2" class="button">
                 <img src="../assets/items/1 (5).png" alt="item2" width="100%">
                 <div class="shop_now">
                     <h4>Buy now!</h4>
                 </div>
             </div>
-        </a>
-
-        <a href="#" id="button" class="button">
+ 
             <div class="item1">
+            <a href="#" id="button3" class="button">
                 <img src="../assets/items/1 (6).png" alt="item2" width="100%">
                 <div class="shop_now">
                     <h4>Buy now!</h4>
                 </div>
             </div>
-        </a>
-
-        <a href="#" id="button" class="button">
+    
             <div class="item1">
+            <a href="#" id="button4" class="button">
                 <img src="../assets/items/1 (7).png" alt="item2" width="100%">
                 <div class="shop_now">
                     <h4>Buy now!</h4>
                 </div>
             </div>
-        </a>
-
-        <a href="#" id="button" class="button">
+  
             <div class="item1">
+            <a href="#" id="button5" class="button">
                 <img src="../assets/items/1 (8).png" alt="item2" width="100%">
                 <div class="shop_now">
                     <h4>Buy now!</h4>
                 </div>
             </div>
-        </a>
 
-            <!-- MODALS -->
-    
+    </div>
 
+    <!-- MODALS -->
     <div class="bg-modal">
         <div class="modal-content">
             <form action="">
                 <div class="close">+</div>
 
-                <a href="#" id="button" class="button">Buy Now!</a>
-                </form>
+            </form>
+        </div>
+    </div>
+
+    <div class="bg-modal1">
+        <div class="modal-content">
+            <form action="">
+                <div class="close1">+</div>
+            </form>
+        </div>
+    </div>
+
+    <div class="bg-modal2">
+        <div class="modal-content">
+            <form action="">
+                <div class="close2">+</div>
+            </form>
+        </div>
+    </div>
+
+    <div class="bg-modal3">
+        <div class="modal-content">
+            <form action="">
+                <div class="close3">+</div>
+            </form>
+        </div>
+    </div>
+
+    <div class="bg-modal4">
+        <div class="modal-content">
+            <form action="">
+                <div class="close4">+</div>
+            </form>
+        </div>
+    </div>
+
+    <div class="bg-modal5">
+        <div class="modal-content">
+            <form action="">
+                <div class="close5">+</div>
+            </form>
         </div>
     </div>
 
