8d4953b1f935427cf4e338a0922736ac3317314d
Mark Ashley Sombillo <markashley423@gmail.com>
2021-02-15 18:14:49 +0800

    changed popup

--- component/shirt.php
+++ component/shirt.php
@@ -16,7 +16,7 @@
             <a href="#" id="button" class="button">
                 <img src="../assets/items/1 (1).png" alt="item2" width="100%">
                 <div class="shop_now">
-                    <h4>Buy now!</h4>
+                    <h4>Buy Now!</h4>
                 </div>
             </a>
             </div>
@@ -25,7 +25,7 @@
             <a href="#" id="button1" class="button">
                 <img src="../assets/items/1 (2).png" alt="item2" width="100%">
                 <div class="shop_now">
-                    <h4>Buy now!</h4>
+                    <h4>Buy Now!</h4>
                 </div>
             </a>
             </div>
@@ -34,7 +34,7 @@
             <a href="#" id="button2" class="button">
                 <img src="../assets/items/1 (5).png" alt="item2" width="100%">
                 <div class="shop_now">
-                    <h4>Buy now!</h4>
+                    <h4>Buy Now!</h4>
                 </div>
             </div>
  
@@ -42,7 +42,7 @@
             <a href="#" id="button3" class="button">
                 <img src="../assets/items/1 (6).png" alt="item2" width="100%">
                 <div class="shop_now">
-                    <h4>Buy now!</h4>
+                    <h4>Buy Now!</h4>
                 </div>
             </div>
     
@@ -50,7 +50,7 @@
             <a href="#" id="button4" class="button">
                 <img src="../assets/items/1 (7).png" alt="item2" width="100%">
                 <div class="shop_now">
-                    <h4>Buy now!</h4>
+                    <h4>Buy Now!</h4>
                 </div>
             </div>
   
@@ -58,7 +58,7 @@
             <a href="#" id="button5" class="button">
                 <img src="../assets/items/1 (8).png" alt="item2" width="100%">
                 <div class="shop_now">
-                    <h4>Buy now!</h4>
+                    <h4>Buy Now!</h4>
                 </div>
             </div>
 
@@ -67,51 +67,200 @@
     <!-- MODALS -->
     <div class="bg-modal">
         <div class="modal-content">
-            <form action="">
+       
+            <div class="popup-buy">
+
+                <div class="description">
+                    <p class="product-name"> Elephant Shirt </p>
+                    <p class="price"> ₱ 500.00 </p>
+                </div>
+                
+                <div class="popup-img">
+                    <img src="../assets/items/1 (1).png" alt="item2" width="100%">
+                </div>
+                
                 <div class="close">+</div>
 
-            </form>
+                <div class="buttons"></div>
+                    <button class="buy-now">Buy Now</button>
+
+                    <button class="add-cart">
+                        Add To Cart
+                        <img class="icon" src="../assets/img/cart.svg" alt="item2" width="100%">
+                    </button>
+                
+                </div>
+
+            </div>
+
+
         </div>
+
     </div>
 
     <div class="bg-modal1">
         <div class="modal-content">
-            <form action="">
+       
+            <div class="popup-buy">
+
+                <div class="description">
+                    <p class="product-name"> Elephant Shirt </p>
+                    <p class="price"> ₱ 500.00 </p>
+                </div>
+                
+                <div class="popup-img">
+                <img src="../assets/items/1 (2).png" alt="item2" width="100%">
+                </div>
+                
                 <div class="close1">+</div>
-            </form>
+
+                <div class="buttons"></div>
+                    <button class="buy-now">Buy Now</button>
+
+                    <button class="add-cart">
+                        Add To Cart
+                        <img class="icon" src="../assets/img/cart.svg" alt="item2" width="100%">
+                    </button>
+                
+                </div>
+
+            </div>
+
+
         </div>
+
     </div>
 
     <div class="bg-modal2">
         <div class="modal-content">
-            <form action="">
+       
+            <div class="popup-buy">
+
+                <div class="description">
+                    <p class="product-name"> Elephant Shirt </p>
+                    <p class="price"> ₱ 500.00 </p>
+                </div>
+                
+                <div class="popup-img">
+                <img src="../assets/items/1 (5).png" alt="item2" width="100%">
+                </div>
+                
                 <div class="close2">+</div>
-            </form>
+
+                <div class="buttons"></div>
+                    <button class="buy-now">Buy Now</button>
+
+                    <button class="add-cart">
+                        Add To Cart
+                        <img class="icon" src="../assets/img/cart.svg" alt="item2" width="100%">
+                    </button>
+                
+                </div>
+
+            </div>
+
+
         </div>
+
     </div>
 
     <div class="bg-modal3">
         <div class="modal-content">
-            <form action="">
+       
+            <div class="popup-buy">
+
+                <div class="description">
+                    <p class="product-name"> Elephant Shirt </p>
+                    <p class="price"> ₱ 500.00 </p>
+                </div>
+                
+                <div class="popup-img">
+                <img src="../assets/items/1 (6).png" alt="item2" width="100%">
+                </div>
+                
                 <div class="close3">+</div>
-            </form>
+
+                <div class="buttons"></div>
+                    <button class="buy-now">Buy Now</button>
+
+                    <button class="add-cart">
+                        Add To Cart
+                        <img class="icon" src="../assets/img/cart.svg" alt="item2" width="100%">
+                    </button>
+                
+                </div>
+
+            </div>
+
+
         </div>
+
     </div>
 
     <div class="bg-modal4">
         <div class="modal-content">
-            <form action="">
+       
+            <div class="popup-buy">
+
+                <div class="description">
+                    <p class="product-name"> Elephant Shirt </p>
+                    <p class="price"> ₱ 500.00 </p>
+                </div>
+                
+                <div class="popup-img">
+                <img src="../assets/items/1 (7).png" alt="item2" width="100%">
+                </div>
+                
                 <div class="close4">+</div>
-            </form>
+
+                <div class="buttons"></div>
+                    <button class="buy-now">Buy Now</button>
+
+                    <button class="add-cart">
+                        Add To Cart
+                        <img class="icon" src="../assets/img/cart.svg" alt="item2" width="100%">
+                    </button>
+                
+                </div>
+
+            </div>
+
+
         </div>
+
     </div>
 
     <div class="bg-modal5">
         <div class="modal-content">
-            <form action="">
+       
+            <div class="popup-buy">
+
+                <div class="description">
+                    <p class="product-name"> Elephant Shirt </p>
+                    <p class="price"> ₱ 500.00 </p>
+                </div>
+                
+                <div class="popup-img">
+                <img src="../assets/items/1 (8).png" alt="item2" width="100%">
+                </div>
+                
                 <div class="close5">+</div>
-            </form>
+
+                <div class="buttons"></div>
+                    <button class="buy-now">Buy Now</button>
+
+                    <button class="add-cart">
+                        Add To Cart
+                        <img class="icon" src="../assets/img/cart.svg" alt="item2" width="100%">
+                    </button>
+                
+                </div>
+
+            </div>
+
+
         </div>
+
     </div>
 
     <script src="../resources/script.js"></script>
