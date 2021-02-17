5f220c58bb9a7c8a7ea5b8a1f510cc06b890047c
Ronald Catibag <catibagronald@gmail.com>
2021-02-17 11:21:26 +0800

    payment commit

---
+++ component/payment.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/design.css?v=<?php echo time(); ?>">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="payment-wrapper">
+
+        <div class="payment-header">
+            <a href="../main.php" class="button">Celyn's Preloved || Shopping Cart</a>
+
+        </div>
+
+        <div class="payment-content">
+
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
