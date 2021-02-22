1e7e7b9ad340edaa91803e6fc1e7b426b2bd9cd7
Ronald Catibag <catibagronald@gmail.com>
2021-02-22 16:02:00 +0800

    merge all tabs into the main

--- main.php
+++ main.php
@@ -30,25 +30,278 @@
                 <img src="./assets/img/background.svg" alt="" srcset="">
                 <div class="about_product">
                     <section id="shirt">
-                        <h1>For shirts only for shirt</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut laborum ea iste saepe. Cumque quo vero, iusto id, dignissimos vel modi illum deleniti provident possimus adipisci fugit. Consequuntur, quaerat.</p>
-                    </section>
-                    <section id="shorts">
-                        <h1>For shirts only for shirt</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut laborum ea iste saepe. Cumque quo vero, iusto id, dignissimos vel modi illum deleniti provident possimus adipisci fugit. Consequuntur, quaerat.</p>
-                    </section>
-                    <section id="jeans">
-                        <h1>For shirts only for shirt</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut laborum ea iste saepe. Cumque quo vero, iusto id, dignissimos vel modi illum deleniti provident possimus adipisci fugit. Consequuntur, quaerat.</p>
-                    </section>
-                    <section id="mugs">
-                        <h1>For shirts only for shirt</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut laborum ea iste saepe. Cumque quo vero, iusto id, dignissimos vel modi illum deleniti provident possimus adipisci fugit. Consequuntur, quaerat.</p>
-                    </section>
+                        <div class="items">
+
+                            <div class="item1">
+                                <a href="#" id="button" class="button">
+                                    <img src="./assets/items/1 (1).png" alt="item2" width="100%">
+                                    <div class="shop_now">
+                                        <h4>Buy Now!</h4>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="item1">
+                                <a href="#" id="button1" class="button">
+                                    <img src="./assets/items/1 (2).png" alt="item2" width="100%">
+                                    <div class="shop_now">
+                                        <h4>Buy Now!</h4>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="item1">
+                                <a href="#" id="button2" class="button">
+                                    <img src="./assets/items/1 (5).png" alt="item2" width="100%">
+                                    <div class="shop_now">
+                                        <h4>Buy Now!</h4>
+                                    </div>
+                            </div>
+
+                            <div class="item1">
+                                <a href="#" id="button3" class="button">
+                                    <img src="./assets/items/1 (6).png" alt="item2" width="100%">
+                                    <div class="shop_now">
+                                        <h4>Buy Now!</h4>
+                                    </div>
+                            </div>
+
+                            <div class="item1">
+                                <a href="#" id="button4" class="button">
+                                    <img src="./assets/items/1 (7).png" alt="item2" width="100%">
+                                    <div class="shop_now">
+                                        <h4>Buy Now!</h4>
+                                    </div>
+                            </div>
+
+                            <div class="item1">
+                                <a href="#" id="button5" class="button">
+                                    <img src="./assets/items/1 (8).png" alt="item2" width="100%">
+                                    <div class="shop_now">
+                                        <h4>Buy Now!</h4>
+                                    </div>
+                            </div>
+
+                        </div>
+
+                        <!-- MODALS -->
+
+                        <div class="bg-modal">
+                            <div class="modal-content">
+
+                                <div class="popup-buy">
+
+                                    <div class="description">
+                                        <p class="product-name"> Elephant Shirt </p>
+                                        <p class="price"> ₱ 500.00 </p>
+                                    </div>
+
+                                    <div class="popup-img">
+                                        <img src="./assets/items/1 (1).png" alt="item2" width="100%">
+                                    </div>
+
+                                    <div class="close">+</div>
+
+                                    <div class="buttons"></div>
+                                    <button class="buy-now">Buy Now</button>
+
+                                    <button class="add-cart">
+                                        Add To Cart
+                                        <img class="icon" src="./assets/img/cart.svg" alt="item2" width="100%">
+                                    </button>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                </div>
+
+                <div class="bg-modal1">
+                    <div class="modal-content">
+
+                        <div class="popup-buy">
+
+                            <div class="description">
+                                <p class="product-name"> Elephant Shirt </p>
+                                <p class="price"> ₱ 500.00 </p>
+                            </div>
+
+                            <div class="popup-img">
+                                <img src="./assets/items/1 (2).png" alt="item2" width="100%">
+                            </div>
+
+                            <div class="close1">+</div>
+
+                            <div class="buttons"></div>
+                            <button class="buy-now">Buy Now</button>
+
+                            <button class="add-cart">
+                                Add To Cart
+                                <img class="icon" src="./assets/img/cart.svg" alt="item2" width="100%">
+                            </button>
+
+                        </div>
+
+                    </div>
+
                 </div>
 
             </div>
+
+            <div class="bg-modal2">
+                <div class="modal-content">
+
+                    <div class="popup-buy">
+
+                        <div class="description">
+                            <p class="product-name"> Elephant Shirt </p>
+                            <p class="price"> ₱ 500.00 </p>
+                        </div>
+
+                        <div class="popup-img">
+                            <img src="./assets/items/1 (5).png" alt="item2" width="100%">
+                        </div>
+
+                        <div class="close2">+</div>
+
+                        <div class="buttons"></div>
+                        <button class="buy-now">Buy Now</button>
+
+                        <button class="add-cart">
+                            Add To Cart
+                            <img class="icon" src="./assets/img/cart.svg" alt="item2" width="100%">
+                        </button>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
         </div>
+
+        <div class="bg-modal3">
+            <div class="modal-content">
+
+                <div class="popup-buy">
+
+                    <div class="description">
+                        <p class="product-name"> Elephant Shirt </p>
+                        <p class="price"> ₱ 500.00 </p>
+                    </div>
+
+                    <div class="popup-img">
+                        <img src="./assets/items/1 (6).png" alt="item2" width="100%">
+                    </div>
+
+                    <div class="close3">+</div>
+
+                    <div class="buttons"></div>
+                    <button class="buy-now">Buy Now</button>
+
+                    <button class="add-cart">
+                        Add To Cart
+                        <img class="icon" src="./assets/img/cart.svg" alt="item2" width="100%">
+                    </button>
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <div class="bg-modal4">
+        <div class="modal-content">
+
+            <div class="popup-buy">
+
+                <div class="description">
+                    <p class="product-name"> Elephant Shirt </p>
+                    <p class="price"> ₱ 500.00 </p>
+                </div>
+
+                <div class="popup-img">
+                    <img src="./assets/items/1 (7).png" alt="item2" width="100%">
+                </div>
+
+                <div class="close4">+</div>
+
+                <div class="buttons"></div>
+                <button class="buy-now">Buy Now</button>
+
+                <button class="add-cart">
+                    Add To Cart
+                    <img class="icon" src="./assets/img/cart.svg" alt="item2" width="100%">
+                </button>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+    </div>
+
+    <div class="bg-modal5">
+        <div class="modal-content">
+
+            <div class="popup-buy">
+
+                <div class="description">
+                    <p class="product-name"> Elephant Shirt </p>
+                    <p class="price"> ₱ 500.00 </p>
+                </div>
+
+                <div class="popup-img">
+                    <img src="./assets/items/1 (8).png" alt="item2" width="100%">
+                </div>
+
+                <div class="close5">+</div>
+
+                <div class="buttons"></div>
+                <button class="buy-now">Buy Now</button>
+
+                <button class="add-cart">
+                    Add To Cart
+                    <img class="icon" src="./assets/img/cart.svg" alt="item2" width="100%">
+                </button>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+    </div>
+
+    <script src="./resources/shirts-script.js"></script>
+
+
+    </section>
+    <section id="shorts">
+        <h1>For shirts only for shirt</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut laborum ea iste saepe. Cumque quo vero, iusto id, dignissimos vel modi illum deleniti provident possimus adipisci fugit. Consequuntur, quaerat.</p>
+    </section>
+    <section id="jeans">
+        <h1>For shirts only for shirt</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut laborum ea iste saepe. Cumque quo vero, iusto id, dignissimos vel modi illum deleniti provident possimus adipisci fugit. Consequuntur, quaerat.</p>
+    </section>
+    <section id="mugs">
+        <h1>For shirts only for shirt</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut laborum ea iste saepe. Cumque quo vero, iusto id, dignissimos vel modi illum deleniti provident possimus adipisci fugit. Consequuntur, quaerat.</p>
+    </section>
+    </div>
+
+    </div>
+    </div>
 
 
     </div>
